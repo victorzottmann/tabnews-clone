bfb7786334394128a88ac276c0473d548de9d996
Victor Zottmann <zottmann.victor@gmail.com>
2023-06-28 06:58:10 +0000

    quick changes

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ function Home() {
   return (
     <div>
       <h1>Oi.</h1>
-      <h3>Saudades de jogar Mario online por várias horas seguidas 😎</h3>
+      <h3>Saudades de jogar Mario online por várias horas seguidas.</h3>
     </div>
   )
 }
